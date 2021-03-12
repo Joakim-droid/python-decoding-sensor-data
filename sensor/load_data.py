b74df1652bd2f741e8fe9e88effbf9d222d55082
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2021-03-12 11:03:38 +0100

    style changes

--- sensor/load_data.py
+++ sensor/load_data.py
@@ -2,6 +2,7 @@ import os
 import glob
 import csv
 
+
 def load_sensor_data():
     sensor_data = []
     sensor_files = glob.glob(os.path.join(os.getcwd(), "datasets", "*.csv"))
@@ -11,6 +12,3 @@ def load_sensor_data():
             for row in data_reader:
                 sensor_data.append(row)
     return sensor_data
-
-
-
